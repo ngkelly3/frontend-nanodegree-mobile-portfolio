8fb9d8d16b8cc7e3dbaa779795674f725b16d0c2
Kelly Ng <kellyng@Kellys-MacBook-Pro.local>
2016-02-24 22:22:59 -0800

    Beautify Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,75 +3,54 @@ var mozjpeg = require('imagemin-mozjpeg'); // pulls from global module library
 
 module.exports = function(grunt) {
 
-// code below breaks the JS
-grunt.initConfig({
-  imagemin: {                          // Task
-    /*static: {                          // Target
-      options: {                       // Target options
-        optimizationLevel: 5,
-        svgoPlugins: [{ removeViewBox: false }],
-        use: [mozjpeg()]
-      },
-      files: {                         // Dictionary of files
-        'build/views/pizzeria.jpg': 'views/images/pizzeria.jpg', // 'destination': 'source'
-        'build/img/profilepic.jpg': 'img/profilepic.jpg'
-      }
-    }, */
+    grunt.initConfig({
 
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 5
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/img',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist/img'
+                }]
+            }
+        },
 
-   dist: {
-      options: {
-        optimizationLevel: 5
-      },
-      files: [{
-         expand: true,
-         cwd: 'img',
-         src: ['**/*.{png,jpg,gif}'],
-         dest: 'build/'
-      }]
-   }
-   },
+        critical: {
+            dist: {
+                options: {
+                    base: './',
+                    css: [
+                        'src/css/style.css',
+                        'src/css/print.css'
+                    ]
+                },
+                // The source file
+                src: 'src/index.html',
+                // The destination file
+                dest: 'dist/index.html'
+            }
+        },
 
-	critical: {
-    dist: {
-      options: {
-        base: './',
-        css: [
-        	'css/style.css',
-        	'css/print.css'
-        ]
-      },
-      // The source file
-      src: 'index.html',
-      // The destination file
-      dest: 'result.html'
-      }
-    },
+        htmlmin: { // Task
+            dist: { // Target
+                options: { // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                    'dist/index.html': 'dist/index.html' // destination: source
+                }
+            },
+        }
+    });
 
-    htmlmin: {                                     // Task
-    dist: {                                      // Target
-      options: {                                 // Target options
-        removeComments: true,
-        collapseWhitespace: true
-      },
-      files: {                                   // Dictionary of files
-        'dist/index.html': 'index.html'	// destination: source
-      }
-    },
-  }
-
-
-
-});
-
-grunt.loadNpmTasks('grunt-contrib-imagemin');
-grunt.loadNpmTasks('grunt-critical');
-grunt.loadNpmTasks('grunt-contrib-htmlmin');
-
-// this JS actually works
-grunt.registerTask('speak', function() {
-	console.log("Hello Bitch");
-});
-
-
+    // load tasks and have them run on "grunt" command
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-critical');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.registerTask('default', ['imagemin', 'critical', 'htmlmin']);
 };
